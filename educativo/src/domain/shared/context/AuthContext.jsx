3d0226294fe06b4e9b9959393575f12225841ed8
josefrancisco <josefrancisco281101@gmail.com>
2024-09-24 08:46:58 -0400

    implementacion del login y rutas protegidas

---
+++ educativo/src/domain/shared/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import {useMutation} from "@tanstack/react-query";
+import { createContext, useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {login} from "../api/authApi";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+
+    const loginMutation = useMutation({
+        mutationKey: ['login'],
+        mutationFn: login,
+
+        onError: (error) => {
+            setErrorMessage(error.response?.data?.message || "Error desconocido");
+        },
+        onSuccess: (data) => {
+            localStorage.setItem("authToken", data.token) || sessionStorage.setItem("authToken", data.token)
+            navigate("/dashboard")
+        }
+    })
+
+    const setUserData = (data) => {
+        setUser(data);
+    }
+    reutn (
+        <AuthContextProvider value={{user, setUserData, loginMutation, errorMessage}}>
+            {children}
+        </AuthContextProvider>
+    )
+}
+
---
+++ educativo/src/domain/shared/pages/ProtectedRoute.jsx
@@ -0,0 +1,40 @@
+import proptypes from 'prop-types';
+import {useQuery} from "@tanstack/react-query";
+import { Navigate } from 'react-router-dom';
+import { getMyInformation } from '../api/authApi';
+import { AuthContext } from '../context/AuthContext';
+import { useContext, useEffect } from 'react';
+
+function ProtectedRoute({children}) {
+    const authToken = localStorage.getItem("authToken");
+    const {setUserData} = useContext(AuthContext);
+    const {data, isError, isLoading} = useQuery({
+        queryKey: ["user"],
+        queryFn: () => getMyInformation(authToken),
+        retry: 1,
+        enabled: Boolean(authToken),
+    })
+
+    useEffect(() => {
+        if (data&& !isError && !isLoading) {
+            setUserData(data);
+            
+        }
+    }, [data, isError, isLoading, setUserData]);
+   if (isLoading) return <div>Cargando...</div>
+
+    
+    if (!authToken || (isError && !data)) {
+        localStorage.removeItem("authToken");
+        console.clear();
+        return <Navigate to="/login" replace={true} />;
+    }
+
+    return children
+    
+}
+
+ProtectedRoute.propTypes = {
+    children: proptypes.any.isRequired,
+  };
+export default ProtectedRoute    
