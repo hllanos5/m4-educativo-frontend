a650ba6dd79a3e31f79d3d834a13aeed53ec1f62
Hans Llanos <hans.llanos@gmail.com>
2024-09-26 17:14:30 -0500

    mejora de menu

--- educativo/src/domain/shared/components/SideBar.jsx
+++ educativo/src/domain/shared/components/SideBar.jsx
@@ -2,10 +2,53 @@ import React, { useContext } from 'react'
 import { TieredMenu } from 'primereact/tieredmenu';
 import {useNavigate} from "react-router-dom";
 import { AuthContext } from '../context/AuthContext';
+
 export default function SideBar() {
     const navigate = useNavigate();
+    
+    const aMenuProfesor = [
+        {
+            label: 'Exámenes',
+            icon: 'pi pi-file-edit',
+            command: () => {
+                navigate("/examen-profesor");
+            }
+        },
+        {
+            label: 'Mis Alumnos',
+            icon: 'pi pi-users',
+            command: () => {
+                navigate("/alumnos-profesor");
+            }
+        },
+        {
+            label: 'Nuevo Examen',
+            icon: 'pi pi-pencil',
+            command: () => {
+                navigate("/examen-crear-profesor");
+            }
+        }
+    ];
+
+    const aMenuAlumno = [
+        {
+            label: 'Exámenes',
+            icon: 'pi pi-file-edit',
+            command: () => {
+                navigate("/examen-alumno");
+            }
+        },
+        {
+            label: 'Mis Videos',
+            icon: 'pi pi-video',
+            command: () => {
+                navigate("/videos-alumno");
+            }
+        }
+    ]
 
     const {user} = useContext(AuthContext);
+    
     let items = [];
     if(user?.role === 'student'){
         items = aMenuAlumno
@@ -20,45 +63,7 @@ export default function SideBar() {
             <TieredMenu model={items} breakpoint="767px" />
         </aside>
     )
+
+
+    
 }
-
-const aMenuProfesor = [
-    {
-        label: 'Exámenes',
-        icon: 'pi pi-file-edit',
-        command: () => {
-            navigate("/examen-profesor");
-        }
-    },
-    {
-        label: 'Mis Alumnos',
-        icon: 'pi pi-users',
-        command: () => {
-            navigate("/alumnos-profesor");
-        }
-    },
-    {
-        label: 'Nuevo Examen',
-        icon: 'pi pi-pencil',
-        command: () => {
-            navigate("/examen-crear-profesor");
-        }
-    }
-];
-
-const aMenuAlumno = [
-    {
-        label: 'Exámenes',
-        icon: 'pi pi-file-edit',
-        command: () => {
-            navigate("/examen-alumno");
-        }
-    },
-    {
-        label: 'Mis Videos',
-        icon: 'pi pi-video',
-        command: () => {
-            navigate("/videos-alumno");
-        }
-    }
-]
