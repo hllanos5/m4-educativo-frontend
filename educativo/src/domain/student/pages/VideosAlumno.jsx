6c27f1715cf1feb7136daa53b360f0b707b92cb9
josefrancisco <josefrancisco281101@gmail.com>
2024-09-27 16:11:23 -0400

    implementacion videos alumnos maestros

--- educativo/src/domain/student/pages/VideosAlumno.jsx
+++ educativo/src/domain/student/pages/VideosAlumno.jsx
@@ -1,34 +1,76 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import axios from 'axios'; 
+import { AuthContext } from '../../shared/context/AuthContext';
 import Layout from '../../shared/layout/Layout';
 import { Card } from 'primereact/card';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../shared/context/AuthContext';
 
 export default function VideosAlumno() {
-    const navigate = useNavigate();
-    const { user } = useContext(AuthContext); 
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  const { user } = useContext(AuthContext);
+//   console.log(user)
+   const getVideos = async (userId, role) => {
+    try {
+      const token = localStorage.getItem("authToken") || sessionStorage.getItem("authToken"); 
+      const res = await axios.get('http://localhost:3000/api/videos', {
+        headers: {
+          Authorization: token 
+        }
+      });
+    //   console.log(token)
+      const allVideos = res.data.data;
+     console.log(allVideos)
+            
+      if (role === 'teacher') {
+        setVideos(allVideos);
+      } else if (role === 'student') {
+      
+        const filteredVideos = allVideos.filter(video => String(video.user) === String(userId));
+        setVideos(filteredVideos);
+       
+        
+       
+      }
+    } catch (error) {
+      setError("Error al cargar los videos");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const handleShowVideo = (videoId) => {
-        navigate(`/video-crear-alumno/${videoId}`); 
-    };
+  useEffect(() => {
+    if (user) {
+      getVideos(user._id, user.role); 
+    }
+  }, [user]);
+  if (loading) return <div>Cargando...</div>;
+  if (error) return <div>{error}</div>;
 
-    return (
-        <Layout>
-            <div className='video-alumno'>
-                {user && user.videos?.length > 0 ? (
-                    user.videos.map((video, i) => (
-                        <Card 
-                            key={i} 
-                            className='card-video' 
-                            onClick={() => handleShowVideo(video.id)}
-                        >
-                            <h2>{video.name}  Video Prueba</h2>
-                        </Card>
-                    ))
-                ) : (
-                    <p>No hay videos disponibles.</p>
-                )}
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1>Videos Disponibles</h1>
+      {videos.length === 0 ? (
+        <p>No hay videos disponibles para mostrar.</p>
+      ) : (
+        <ul>
+          {videos.map(video => (
+            <li key={video._id}>
+              <Card title={`Video: ${video.video}`}>
+                <video controls>
+                  <source src={`http://localhost:3000/uploads/${video.video}`} type="video/mp4" />
+                  Tu navegador no soporta la reproducción de este video.
+                </video>
+              </Card>
+              
+            </li> 
+            
+          )) }
+         
+        </ul>
+      )}
+     
+    </Layout>
+  );
 }
